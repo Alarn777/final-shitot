76d1aaed22a0149baecb4429acd4dcb00fb6c1a5
Michael <alarn777@gmail.com>
2019-07-29 09:27:33 +0300

    panel demo

--- FinalProject/Source.cpp
+++ FinalProject/Source.cpp
@@ -1,13 +1,18 @@
 #include "../Common/Graphics.h"
-#include "../Controls/Label.h"
 #include "../Common/EventEngine.h"
+#include "../Controls/Panel.h"
+
+Panel *mainPanel;
+
+Border *border = new Border();
 
 int main(int argc, char** argv)
 {
 
-	Label l("Hello World");
-	Control& l2 = l;
+  	mainPanel = new Panel(0, 0, 2, 3+2, border, Color::White, Color::Blue);
+    mainPanel->setName("Console GUI Application");
+
 	EventEngine e;
-	e.run(l);
+	e.run(*mainPanel);
 
 }

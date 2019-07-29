e224fd4240abccc95c878a49baf672627905d451
Michael <alarn777@gmail.com>
2019-07-29 08:12:03 +0300

    control started

--- Common/Control.h
+++ Common/Control.h
@@ -10,12 +10,22 @@ class Control
 protected:
 	short left;
 	short top;
+	short width;
+	short height;
+	Color textColor;
+	Color backgroundColor;
 	
 public:
 	Control();
 	static Control* getFocus() { return NULL; };
 	static void setFocus(Control& control) {};
 	
+	void setTextColor(Color textColor) { this->textColor = textColor; };
+	virtual void setLeft(short left) { this->left = left; };
+	virtual void setTop(short top) { this->top = top; };
+	virtual void setWidth(short width) { this->width = width; };
+	virtual void setHeight(short height) { this->height = height; };	
+	void setBackgroundColor(Color backgroundColor) { this->backgroundColor = backgroundColor; };
 	
 
 	virtual void draw(Graphics& g, int x, int y, size_t z) {};
